8fc320b68cf9ea529b718140023929e0a295fd14
Mustafizur Rahaman <61288061+MustfizurRahaman@users.noreply.github.com>
2024-01-24 08:45:51 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+var map = L.map('map').setView([37.7749, -122.4194], 13);
+
+var esriStreets = L.esri.basemapLayer('Streets').addTo(map);
+
+$.getJSON("https://raw.githubusercontent.com/orhuna/WebGIS_SLU_M1/main/Module%201/Assignment%201/data/sf_crime.geojson",function(data){
+    var customIcon = L.icon({
+      iconUrl: 'https://d1nhio0ox7pgb.cloudfront.net/_img/i_collection_png/512x512/plain/criminal.png',
+      iconSize: [60,60]
+    });
+    var markers = L.geoJson(data, {
+      pointToLayer: function(feature,latlng){
+        var marker = L.marker(latlng,{icon: customIcon});
+        marker.bindPopup(feature.properties.Location + '<br/>' + feature.properties.OPEN_DT + '<br/>' + feature.properties.SUBJECT);
+        return marker;
+      }
+    });
+    var clusters = L.markerClusterGroup();
+    clusters.addLayer(markers);
+    map.addLayer(clusters);
+});
